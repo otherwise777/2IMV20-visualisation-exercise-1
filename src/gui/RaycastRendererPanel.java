4b1d1fb0b4f24e4210a7370284a0dea826169db0
otherwise777 <commits@onzichtbaar.net>
2015-11-28 19:22:17 +0100

    .

--- src/gui/RaycastRendererPanel.java
+++ src/gui/RaycastRendererPanel.java
@@ -33,6 +33,7 @@ public class RaycastRendererPanel extends javax.swing.JPanel {
     }
     
     
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
